3b994e96a9fa3a997dff9654475f334d96698a18
Harsh Mandan <harshsinghmandan@gmail.com>
2018-05-30 13:04:14 +0530

    Updated env port for dynamic binding

--- server.js
+++ server.js
@@ -106,6 +106,6 @@ app.post('/', function (req, res) {
 	res.render('markup', {newmarkup, newmarkup, error: null});
 })
 
-app.listen(3000, function () {
-  console.log('Markup app listening on port 3000!')
+app..listen(process.env.PORT || 5000, function () {
+  console.log('Markup app listening on port 5000!')
 })
